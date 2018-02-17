8b740f9f951871f84a3ca679158a0af01e669ae1
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-17 15:24:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element){
+var function addElementToBeginningOfArray(array, element){
   array = [array];
   element = 'foo';
   if(element == 'foo'){
