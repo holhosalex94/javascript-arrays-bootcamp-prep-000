b7f814c4353a3a365603732f9b8f5cf8ca27b93d
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-17 15:22:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
   array = [array];
-  if(element !== undefined){
+  if(element == 'foo'){
    array = array.unshift(element);
   }
 }
